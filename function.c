ced5d66b04b57720e0877a38314309b67cbcb9cf
Haoran <bofengco@foxmail.com>
2021-03-06 23:37:03 +0800

    Create function.c

---
+++ function.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int max(int num1, int num2);
+
+int main (void)
+{
+
+    int a = 100;
+    int b = 200;
+    int ans;
+
+    ans = max(a, b);
+
+    printf( "Max value is : %d\n", ans);
+
+    return 0;
+}
+
+
+int max(int num1, int num2)
+{
+    int result;
+
+    if (num1 > num2)
+        result = num1;
+    else
+        result = num2;
+
+    return result;
+}
