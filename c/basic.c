3f44e55d63da059e66f3225b839581eb20863ae0
Haoran <bofengco@foxmail.com>
2021-03-06 23:18:44 +0800

    Update basic.c

--- c/basic.c
+++ c/basic.c
@@ -1,6 +1,22 @@
+// bash environment build & execute:
+//
+// gcc test1.c test2.c -o main.out
+// ./main.out
+
 #include <stdio.h>
 
-int main() {
+int main(void) {
+
+    char ch = 'c';
+    putchar(ch);
+
+    int num1, num2, num3;
+    const double num = 1.1;
+
+    register int i = 1;
+
+    printf("int 存储大小 : %lu \n", sizeof(int));
+
     printf("Hello, World!\n");
     return 0;
 }
