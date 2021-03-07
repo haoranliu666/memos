58d9fe135e6c91c345836a0bcc10a98e973966b7
Haoran <bofengco@foxmail.com>
2021-03-07 13:28:02 +0800

    Create malloc.c

---
+++ c/malloc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[100];
+    char *description;
+
+    strcpy(name, "Zara Ali");
+
+    //malloc
+    description = (char *)malloc( 30 * sizeof(char) );
+    if( description == NULL )
+    {
+        fprintf(stderr, "Error - unable to allocate required memory\n");
+    }
+    else
+    {
+        strcpy( description, "Zara ali a DPS student.");
+    }
+
+
+    //realloc
+    description = (char *) realloc( description, 100 * sizeof(char) );
+    if( description == NULL )
+    {
+        fprintf(stderr, "Error - unable to allocate required memory\n");
+    }
+    else
+    {
+        strcat( description, "She is in class 10th");
+    }
+
+    printf("Name = %s\n", name );
+    printf("Description: %s\n", description );
+
+    free(description);
+    
+    return 0;
+    
+}
