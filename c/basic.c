a0d7a9b2af47290d4ed0ef9b5eab30c69263c722
Haoran <bofengco@foxmail.com>
2021-03-11 10:29:05 +0800

    Update basic.c

--- c/basic.c
+++ c/basic.c
@@ -13,9 +13,23 @@ int main(void) {
     bool a = true;
     int num1, num2, num3;
     const double num = 1.1;
+    
+    //force type change
+    int a = (int)1.0
 
-    register int i = 1;
-
+    //pointer
+    int  var = 20;
+    int  *ip = NULL;
+    ip = &var;
+    
+    //pointer array
+    const char *names[] = {
+                   "Zara Ali",
+                   "Hina Ali",
+                   "Nuha Ali",
+                   "Sara Ali",
+    };
+    
     printf("int 存储大小 : %lu \n", sizeof(int));
 
     printf("Hello, World!\n");
