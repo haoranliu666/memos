a32975986963b459b166370d8ae8eb527330b4e0
Haoran <bofengco@foxmail.com>
2021-03-11 11:00:34 +0800

    Update basic.cpp

--- cpp/basic.cpp
+++ cpp/basic.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main()
 {
     cout << "Hello, world!" << endl;
+    
+    //reference, another name of the variable
+    int i = 17;
+    int &  r = i;
+    r++;
+    
+    cout << i << endl;
+    
+    
     return 0;
 }
