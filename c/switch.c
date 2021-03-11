c9b3281dea272c6dd0e1d5f09d6a4251e8ee4cb8
Haoran <bofengco@foxmail.com>
2021-03-11 21:06:00 +0800

    Update switch.c

--- c/switch.c
+++ c/switch.c
@@ -1,1 +1,28 @@
-//https://www.runoob.com/cprogramming/c-switch.html
+#include <stdio.h>
+ 
+int main ()
+{
+   char grade = 'B';
+ 
+   switch(grade)
+   {
+   case 'A' :
+      printf("Excellent\n" );
+      break;
+   case 'B' :
+   case 'C' :
+      printf("Good\n" );
+      break;
+   case 'D' :
+      printf("Not bad\n" );
+      break;
+   case 'F' :
+      printf("Try again\n" );
+      break;
+   default :
+      printf("Nonsense\n" );
+   }
+   printf("Your grade is %c\n", grade );
+ 
+   return 0;
+}
