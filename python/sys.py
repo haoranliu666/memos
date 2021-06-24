5b97db90554c86560aa52503a5f43af2b29b3e41
Haoran <bofengco@foxmail.com>
2021-06-24 17:55:05 +0800

    Update sys.py

--- python/sys.py
+++ python/sys.py
@@ -4,3 +4,19 @@ import sys
 
 n = sys.argv[1]
 n = int(n)
+
+
+#批量改名
+import os
+
+path = '/Users/haoranliu/Desktop/esginfo/xls'
+file_list = os.listdir(path)
+
+newpath = '/Users/haoranliu/Desktop/esginfo/xml'
+
+for file_name in file_list:
+    if file_name == ".DS_Store":
+        continue
+    oldname = path + os.sep + file_name
+    newname = newpath + os.sep + file_name + '.xml'
+    os.rename(oldname,newname)
