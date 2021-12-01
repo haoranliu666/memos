773980825bf66a495c776cd5390dc51a9eef2c60
Haoran <bofengco@foxmail.com>
2021-12-01 15:18:11 +0800

    Update requests.py

--- python/requests.py
+++ python/requests.py
@@ -1,3 +1,11 @@
 import requests
 
 #https://stackoverflow.com/questions/23651947/python-requests-requests-exceptions-toomanyredirects-exceeded-30-redirects
+
+url = 'baidu.com'
+
+headers = {
+    'User-Agent':'xxxx'
+}
+
+r = requests.get(url, headers = headers)
