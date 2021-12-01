c1398b8b9066f5c6130a2076ef32cc8171486b54
Haoran <bofengco@foxmail.com>
2021-12-01 13:37:16 +0800

    Update json.py

--- python/json.py
+++ python/json.py
@@ -7,3 +7,48 @@ with open('aaa.json', 'r') as handle:
 #save
 with open('bbb.json', 'w') as handle:
     json.dump(bbb, handle, indent=2)
+
+    
+
+#an example
+{
+  "squadName": "Super hero squad",
+  "homeTown": "Metro City",
+  "formed": 2016,
+  "secretBase": "Super tower",
+  "active": true,
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
