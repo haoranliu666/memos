3fb08385a7af3ae2f0ffc10b10fc53141b26e653
Haoran <haoran_us@outlook.com>
2023-01-19 13:03:56 -0500

    Update 1basic.py

--- python/1basic.py
+++ python/1basic.py
@@ -150,3 +150,53 @@ except:
 
 
 
+####### iteration
+
+c = ["Mark", "Duck", "Hey"]
+for name in c:
+    print(name)
+    
+d = {'a': 1, 'b': 2, 'c': 3}
+for key in d:
+    print(key)
+
+for ch in 'ABC':
+    print(ch)
+
+
+####### list comprehensions
+
+list(range(1, 11))
+
+[x * x for x in range(1, 11)]
+
+[x * x for x in range(1, 11) if x % 2 == 0]
+
+[m + n for m in 'ABC' for n in 'XYZ']  # double loop
+
+
+
+####### generator
+
+g = (x * x for x in range(10))
+
+next(g)
+
+for n in g:
+    print(n)
+
+#fibonacci
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+f=fib(10)
+next(f)
+
+#loop with both index and content:
+for i, t in enumerate( ['Haoran', 'Liu', 'Macbook']):
+    print(i)
+    print(i)
